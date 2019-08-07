54396e0c6f32b4798b777cfadcc5aac87ea20a29
Jordan Holt <jordan.holt@bbc.co.uk>
2019-08-07 15:05:33 +0100

    Move logging to middleware

--- lib/client.ts
+++ lib/client.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 import uuidv4 from 'uuid/v4';
 import compose from 'koa-compose';
+import { logging } from './middleware/logging';
 import { caching } from './middleware/caching';
 import { circuit } from './middleware/circuitBreaker';
 import { isValidJson } from './middleware/validJson';
@@ -21,96 +22,13 @@ const request = (client: HttpClient, config: RequestConfig) => {
   config.headers = config.headers || {};
   config.headers['X-Correlation-Id'] = requestId;
 
-  const logParts = { url: config.url, client: client._config.name, type: 'upstream', requestId };
-
-  const successHandler = (ctx) => {
-    // post-response (success): isValidJson, tryCache
-    console.log('response: ', ctx.response);
-    let logData = logHandler(logParts, ctx.response);
-    client._config.logger.info(logData);
-    return ctx.response.data;
-  }
-
-  const errorHandler = (ctx) => {
-    // post-response (error): openCircuit
-
-    let customError = {}
-    if (ctx.response && ctx.response.status) {
-      customError = {
-        name: `ESTATUS${ctx.response.status}`,
-        message: `Status code ${ctx.response.status} received for ${config.url}`,
-        details: ctx.message || ''
-      }
+  const context = {
+    client: { name: client._config.name },
+    request: {
+      id: requestId,
+      ...config
     }
-    else if (ctx.name && ctx.name === 'ECIRCUITBREAKER') {
-      customError = {
-        name: ctx.name,
-        message: ctx.message
-      }
-    }
-    else if (ctx.code && ctx.code === 'ECONNABORTED') {
-      customError = {
-        name: ctx.code,
-        message: ctx.message
-      }
-    }
-    else {
-      customError = {
-        name: `EUNKNOWN`,
-        message: ctx.message
-      }
-    }
-
-    let logData = logHandler(logParts, ctx.response);
-    return { ...customError, ...logData }
-  }
-
-  const logHandler = (logParts, response) => {
-    let duration = response.headers['x-response-time'];
-    let statusCode = response.status;
-    let contentLength = response.headers['content-length'];
-
-    return {
-      ...logParts, duration, statusCode, contentLength
-    }
-  }
-
-  const func1 = async (ctx, next) => {
-    console.log('func1 ctx: ', ctx);
-    console.log(1);
-
-    // testing short-circuit response
-    const res = {
-      response: {
-        headers: {
-          'cache-control': 'no-cache'
-        },
-        data: '<lol></lol>'
-      }
-    };
-
-    return res;
-
-    await next();
-
-    // console.log('func2 2nd invoc ctx:', ctx);
-    console.log(4);
-    return ctx;
-  }
-
-  const func2 = async (ctx, next) => {
-    console.log('func2 ctx: ', ctx);
-    console.log(2);
-
-    await next();
-
-    // console.log('func2 2nd invoc ctx:', ctx);
-    console.log(3);
-    return ctx;
-  }
-
-  client._middleware.push(func1);
-  client._middleware.push(func2);
+  };
 
   const composedMiddleware = compose(client._middleware);
 
@@ -120,9 +38,18 @@ const request = (client: HttpClient, config: RequestConfig) => {
       .catch((err) => ctx.error = err);
   }
 
-  return composedMiddleware({ request: config }, doRequest)
-    .then(successHandler)
-    .catch(errorHandler);
+  return composedMiddleware(context, doRequest)
+    .then((ctx) => ctx.response.data)
+    .catch((ctx) => {
+      if (!ctx.error) {
+        ctx.error = {
+          name: `EUNKNOWN`,
+          message: ctx.message || ''
+        }
+      }
+
+      return ctx.error;
+    });
 }
 
 class HttpClient {
@@ -133,6 +60,10 @@ class HttpClient {
   constructor(config: HttpClientConfig) {
     this._config = { ...defaults, ...config };
     this._middleware = [];
+
+    if (this._config.logger) {
+      this._middleware.push(logging(this._config.logger));
+    }
 
     this._agent = axios.create({
       timeout: this._config.timeout,
