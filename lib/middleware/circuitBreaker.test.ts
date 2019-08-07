bb4b796d98624fe5c184d573940d8945840347d6
Emilian Patrasc <emilian.patrasc@bbc.co.uk>
2019-08-07 19:34:16 +0100

    implement some basic test for circuit breaker

--- lib/middleware/circuitBreaker.test.ts
+++ lib/middleware/circuitBreaker.test.ts
@@ -1,7 +1,29 @@
-import { circuit } from './circuitBreaker';
+import { circuitBreaker } from './circuitBreaker';
+
+jest.mock('opossum', () => () => ({ opened: true, fire: () => Promise.resolve() }))
+
+const context: MiddlewareContext = {
+    client: {
+        name: 'client-name'
+    },
+    request: { method: 'get', url: 'foo' }
+}
 
 describe('circuit breaker', () => {
-    it.skip('fake',()=>{
+    describe('when circuit is open', () => {
+        it('should add an error to the context', async () => {
+            const circuit = circuitBreaker({ errorThresholdPercentage: Infinity, resetTimeout: 0 })
+            const next = jest.fn();
 
+            await circuit(context, next)
+
+            const result = {
+                message: "Circuit breaker is open for client-name",
+                name: "ECIRCUITBREAKER"
+            }
+
+            expect(next).not.toBeCalled()
+            expect(context.error).toEqual(result)
+        })
     })
 });
