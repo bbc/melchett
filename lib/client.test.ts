52efa710703f5b773f491b44c0fd8d2245ce6540
Jordan Holt <jordan.holt@bbc.co.uk>
2019-08-19 14:35:10 +0100

    Add unit tests for client

--- lib/client.test.ts
+++ lib/client.test.ts
@@ -1,12 +1,210 @@
-import nock from 'nock';
-import { HttpClient } from './client';
+import { HttpClient, request } from './client';
+import axios from 'axios';
+import uuidv4 from 'uuid/v4';
+import compose from 'koa-compose';
+import * as caching from './middleware/caching';
+import * as circuitBreaker from './middleware/circuitBreaker';
+import * as validStatus from './middleware/validStatus';
+import * as settleResponse from './utils/settleResponse';
+
+jest.mock('axios');
+jest.mock('uuid/v4');
+jest.mock('koa-compose');
+jest.mock('./middleware/caching');
+jest.mock('./middleware/circuitBreaker');
+jest.mock('./middleware/validStatus');
+jest.mock('./middleware/validJson');
+jest.mock('./utils/settleResponse');
+
+const mockAxios = axios as jest.Mocked<typeof axios>;
+const mockUuid = uuidv4 as jest.Mocked<typeof uuidv4>;
+const mockCompose = compose as jest.Mocked<typeof compose>;
+const mockCaching = caching as jest.Mocked<typeof caching>;
+const mockCircuitBreaker = circuitBreaker as jest.Mocked<typeof circuitBreaker>;
+const mockValidStatus = validStatus as jest.Mocked<typeof validStatus>;
+const mockSettleResponse = settleResponse as jest.Mocked<typeof settleResponse>;
 
 describe('client', () => {
-    const client = new HttpClient({ name: 'foo' })
-    
-    describe('when "get" is called', () => {
-        it('should return a response', () => {
-            
-        })
-    })
+    describe('constructor', () => {
+        it('should add cache middleware if option set', async () => {
+            // Arrange
+            const config = {
+                name: 'test',
+                cache: {
+                    store: undefined
+                }
+            };
+
+            // Act
+            new HttpClient(config);
+
+            // Assert
+            expect(mockCaching.caching).toBeCalledTimes(1);
+        });
+
+        it('should add circuit breaker middleware to stack top if option set', async () => {
+            // Arrange
+            const config = {
+                name: 'test',
+                circuitBreaker: {
+                    errorThresholdPercentage: 0.5,
+                    resetTimeout: 30
+                }
+            };
+
+            // Act
+            new HttpClient(config);
+
+            // Assert
+            expect(mockCircuitBreaker.circuitBreaker).toBeCalledTimes(1);
+        });
+
+        it('should add common middleware', async () => {
+            // Arrange
+            const config = {
+                name: 'test'
+            };
+
+            // Act
+            new HttpClient(config);
+
+            // Assert
+            expect(mockValidStatus.validStatus).toBeCalledTimes(1);
+        });
+
+        it('should compose middleware', async () => {
+            // Arrange
+            const config = {
+                name: 'test'
+            };
+
+            // Act
+            new HttpClient(config);
+
+            // Assert
+            expect(mockCompose).toBeCalledTimes(1);
+        });
+
+        it('should setup axios with correct options', async () => {
+            // Arrange
+            const config = {
+                name: 'test'
+            };
+
+            // Act
+            new HttpClient(config);
+
+            // Assert
+            expect(mockAxios.create).toBeCalledTimes(1);
+        });
+    });
+
+    describe('request', () => {
+        beforeEach(() => {
+            mockCompose.mockImplementation(() => async (context, finalFunc) => {
+                try {
+                    await finalFunc();
+                } catch (ex) {}
+                return Promise.resolve()
+            });
+            mockUuid.mockImplementation(() => 'test-uuid');
+        });
+
+        it('should generate request ID', async () => {
+            // Arrange
+            const clientConfig: HttpClientConfig = {
+                name: 'test'
+            };
+
+            const requestConfig: RequestConfig = {
+                method: 'get',
+                url: 'https://www.bbc.co.uk'
+            };
+
+            // Act
+            request(new HttpClient(clientConfig), requestConfig);
+
+            // Assert
+            expect(mockUuid).toBeCalledTimes(1);
+        });
+
+        it('should call request on axios agent', async () => {
+            // Arrange
+            const clientConfig: HttpClientConfig = {
+                name: 'test'
+            };
+
+            const requestConfig: RequestConfig = {
+                method: 'get',
+                url: 'https://www.bbc.co.uk'
+            };
+
+            const mockAgentRequest = jest.fn();
+
+            const client = new HttpClient(clientConfig);
+            client._composedMiddleware = jest.fn();
+            client._composedMiddleware.mockReturnValue(Promise.resolve({}));
+            client._agent = {
+                request: mockAgentRequest
+             } as any;
+
+            // Act
+            request(new HttpClient(clientConfig), requestConfig);
+
+            // Assert
+            expect(mockAgentRequest).toBeCalledTimes(1);
+        });
+
+        it('should call composedMiddleware with expected context', async () => {
+            // Arrange
+            const clientConfig: HttpClientConfig = {
+                name: 'test'
+            };
+
+            const requestConfig: RequestConfig = {
+                method: 'get',
+                url: 'https://www.bbc.co.uk'
+            };
+
+            const client = new HttpClient(clientConfig);
+            client._composedMiddleware = jest.fn();
+            client._composedMiddleware.mockReturnValue(Promise.resolve({}));
+
+            const expectedContext = {
+                client: { name: 'test' },
+                request: {
+                    headers: {
+                        'X-Correlation-Id': 'test-uuid'
+                    },
+                    id: 'test-uuid',
+                    method: 'get',
+                    url: 'https://www.bbc.co.uk'
+                }
+            }
+
+            // Act
+            request(client, requestConfig);
+
+            // Assert
+            expect(client._composedMiddleware).toBeCalledWith(expectedContext, expect.any(Function));
+        });
+
+        it('should call settleResponse', async () => {
+            // Arrange
+            const clientConfig: HttpClientConfig = {
+                name: 'test'
+            };
+
+            const requestConfig: RequestConfig = {
+                method: 'get',
+                url: 'https://www.bbc.co.uk'
+            };
+
+            // Act
+            request(new HttpClient(clientConfig), requestConfig);
+
+            // Assert
+            expect(mockSettleResponse.settleResponse).toBeCalled();
+        });
+    });
 });
