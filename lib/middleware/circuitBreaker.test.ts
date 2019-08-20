68554059b4faa3bd8515df53facf6fb8c6c8d6f2
Emilian Patrasc <emilian.patrasc@bbc.co.uk>
2019-08-20 21:07:43 +0100

    fix circuitbreaker tests

--- lib/middleware/circuitBreaker.test.ts
+++ lib/middleware/circuitBreaker.test.ts
@@ -1,35 +1,34 @@
-import opossum from 'opossum';
 import { circuitBreaker } from './circuitBreaker';
 
 const defaultContext: MiddlewareContext = {
-    client: {
-        name: 'client-name'
-    },
-    request: { method: 'get', url: 'foo' }
+    client: { name: 'client-name' },
+    request: { method: 'get', url: 'foo' },
+    response: { status: undefined }
 }
 
-jest.mock('opossum');
+const mockOpossum = { opened: false, fire: () => Promise.resolve() };
+jest.mock('opossum', () => () => mockOpossum);
 
 describe('Circuit breaker', () => {
     describe('when circuit is open', () => {
         let context;
 
         beforeEach(() => {
-            jest.mock('opossum', () => () => ({ opened: true, fire: () => Promise.resolve() }));
             context = { ...defaultContext };
+            mockOpossum.opened = true;
         });
 
         it('should add an error to the context', async () => {
             const handler = circuitBreaker({ errorThresholdPercentage: Infinity, resetTimeout: 0 });
             const next = jest.fn();
-            
+
             const expected = {
                 message: "Circuit breaker is open for client-name",
                 name: "ECIRCUITBREAKER"
             };
 
             await handler(context, next);
-            
+
             expect(next).not.toBeCalled();
             expect(context.error).toEqual(expected);
         });
@@ -39,8 +38,8 @@ describe('Circuit breaker', () => {
         let context;
 
         beforeEach(() => {
-            jest.mock('opossum', () => () => ({ opened: false, fire: () => Promise.resolve() }));
             context = { ...defaultContext };
+            mockOpossum.opened = false;
         });
 
         it('should call the next function and context NOT have errors', async () => {
